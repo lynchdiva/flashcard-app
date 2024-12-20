a863020391b3d7555f50f3f4b323e8d127d2fdab
lynchdiva <attamaill@gmail.com>
2024-12-20 18:54:31 +0400

    init: repo

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.scss';
+import React from 'react';
+import Header from './assets/components/Header/Header.jsx';
+import Main from './assets/components/Main/Main.jsx';
+import Footer from './assets/components/Footer/Footer.jsx';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <Main />
+      <Footer />
+    </React.Fragment>
+  );
+}
+
+export default App;
