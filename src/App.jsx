cb1e2977674558f8db62f8a65f79fd59ddc7d0e7
lynchdiva <attamaill@gmail.com>
2025-03-04 15:01:17 +0400

    feat: server proxy

--- src/App.jsx
+++ src/App.jsx
@@ -6,87 +6,15 @@ import { useState } from 'react';
 import { Routes, Route } from 'react-router';
 
 function App() {
-  //хранить массив слов в локальном состоянии и передать обработчик к дочерним компонентам для изменения этого массива - временное решение до подключения менеджера состояния
-  const data = [
-    {
-      id: '33026',
-      english: 'precise',
-      transcription: '[prɪˈsaɪs]',
-      russian: 'точный',
-      tags: ''
-    },
-    {
-      id: '33027',
-      english: 'analysis',
-      transcription: '[əˈnæləsɪs]',
-      russian: 'анализ',
-      tags: ''
-    },
-    {
-      id: '33028',
-      english: 'approach',
-      transcription: '[əˈprəʊtʃ]',
-      russian: 'подход',
-      tags: ''
-    },
-    {
-      id: '33029',
-      english: 'resilience',
-      transcription: '[rɪˈzɪlɪəns]',
-      russian: 'устойчивость',
-      tags: ''
-    },
-    {
-      id: '33047',
-      english: 'pacification',
-      transcription: '[ˌpæsəfəˈkeɪʃən]',
-      russian: 'умиротворение',
-      tags: ''
-    },
-    {
-      id: '33050',
-      english: 'persistent',
-      transcription: '[pərˈsɪstənt]',
-      russian: 'упорный',
-      tags: ''
-    },
-    {
-      id: '33051',
-      english: 'flabbergasted',
-      transcription: '[ˈflæbərˌɡæstəd]',
-      russian: 'ошеломлённый',
-      tags: ''
-    },
-    {
-      id: '33067',
-      english: 'kaiser',
-      transcription: '[ˈkaɪzər]',
-      russian: 'кайзер',
-      tags: ''
-    },
-    {
-      id: '33073',
-      english: 'melodious',
-      transcription: '[mɪˈloʊdɪəs]',
-      russian: 'мелодичный, певучий',
-      tags: ''
-    },
-    {
-      id: '33074',
-      english: 'tidily',
-      transcription: '[ˈtaɪdɪli] ',
-      russian: 'опрятно',
-      tags: ''
-    }
-  ];
-
-  const [words, changeWords] = useState(data || []);
+  const [words, setWords] = useState([]);
+  const [learnedWords, setLearnedWords] = useState([]);
+  const [inProgressWords, setInProgressWords] = useState([]);
 
   const handleWordsChange = replacement => {
     for (let key in replacement) {
       replacement[key] = replacement[key].trim();
     }
-    changeWords(
+    setWords(
       words.map(originalWord => {
         return originalWord.id === replacement.id ? replacement : originalWord;
       })
@@ -94,7 +22,7 @@ function App() {
   };
 
   const handleDeleteWord = word => {
-    changeWords(words.filter(originalWord => originalWord.id !== word.id));
+    setWords(words.filter(originalWord => originalWord.id !== word.id));
   };
 
   return (
